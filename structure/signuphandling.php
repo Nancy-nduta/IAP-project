b10050dbe80b30cd80de2682d057b59df9d73f1f
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 23:49:04 +0300

    created a query to insert data into the users table

--- structure/signuphandling.php
+++ structure/signuphandling.php
@@ -13,3 +13,5 @@ $phone_number = $_POST['phone_number'];
 
 // Hash the password
 $password = password_hash($password, PASSWORD_DEFAULT);
+// Create a query to insert the data into the users table
+$query = "INSERT INTO users (fullname, email, username, password, genderId, roleId, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?)";
