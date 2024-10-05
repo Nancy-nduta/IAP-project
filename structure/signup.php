25aa3e2e012c5864c600499d68c8903a33ae234b
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 23:31:34 +0300

    added role field

--- structure/signup.php
+++ structure/signup.php
@@ -30,3 +30,17 @@
      <option value="1">Female</option>
     </select>
   </div>
+  <div class="form-group">
+   <label for="roleId">Role:</label>
+   <select class="form-control" id="roleId" name="roleId" required>
+    <option value="0">User </option>
+      <option value="1">Admin</option>
+   </select>
+  </div>
+ <div class="form-group">
+    <label for="phone_number">Phone Number:</label>
+    <input type="tel" class="form-control" id="phone_number" name="phone_number" required>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign up</button>
+</form>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cosmo/bootstrap.min.css">
