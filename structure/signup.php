0f7fba66ba9b0650fd6747e8d1da29ab7ba86f55
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 22:47:36 +0300

    created full name input field

---
+++ structure/signup.php
@@ -0,0 +1,13 @@
+<?php
+ // Include the menus.php file to display the main menu
+ include_once 'menus.php';
+ $menu = new menus();
+ $menu->main_menu();
+?>
+
+<h1>Sign up</h1>
+<form action="signuphandling.php" method="post">
+  <div class="form-group">
+   <label for="fullname">Full Name:</label>
+    <input type="text" class="form-control" id="fullname" name="fullname" required>
+  </div>
