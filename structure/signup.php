79fe3ea68ea5409aef55b93aa0bf477963e5415c
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 22:52:16 +0300

    created password field

--- structure/signup.php
+++ structure/signup.php
@@ -19,3 +19,7 @@
   <label for="username">Username:</label>
    <input type="text" class="form-control" id="username" name="username" required>
  </div>
+ <div class="form-group">
+24    <label for="password">Password:</label>
+25    <input type="password" class="form-control" id="password" name="password" required>
+26  </div>
