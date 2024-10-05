9c5b7faacc97c96065b96bfa1747ce4f8b0167ad
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 22:50:46 +0300

    created username field

--- structure/signup.php
+++ structure/signup.php
@@ -15,3 +15,7 @@
    <label for="email">Email:</label>
     <input type="email" class="form-control" id="email" name="email" required>
   </div>
+  <div class="form-group">
+20    <label for="username">Username:</label>
+21    <input type="text" class="form-control" id="username" name="username" required>
+22  </div>
