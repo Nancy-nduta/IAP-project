d967c82b45e321dc32937aa337bb3cd9461c9cd8
nduta <142427559+Nancy-nduta@users.noreply.github.com>
2024-10-05 23:47:48 +0300

    added code for hashing the password

--- structure/signuphandling.php
+++ structure/signuphandling.php
@@ -1,3 +1,15 @@
 <?php
 // Include the database connection file
 include_once 'db_connection.php';
+
+// Get the form data
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$genderId = $_POST['genderId'];
+$roleId = $_POST['roleId'];
+$phone_number = $_POST['phone_number'];
+
+// Hash the password
+$password = password_hash($password, PASSWORD_DEFAULT);
